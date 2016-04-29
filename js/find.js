889e7d7ae4c44a0787ce1893c1ab6dfe6d5a9d0c
Alec Ramos <alec1034780@icloud.com>
2016-04-28 21:00:32 -0700

    Add Things

---
+++ js/find.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+
+	$('#search').on('click', results);
+	$('#result').hide();
+
+	function results(event) {
+		 profile();
+		 repos(); 
+	}
+
+	function profile (event) {
+		var name = $('#githubName').val();
+		$('#result').show();
+		$.getJSON('https:/api.github.com/users/' + name, function(data){
+			console.log(data);
+			$('#location').text(data.location);
+			$('#name').text(data.name);
+			$('#username').text(data.login);
+			$('#followers').text(data.followers);
+			$('#following').text(data.following);
+			$('#avatar').attr('src', data.avatar_url);
+			$('#blog_url').attr('href', data.blog);
+			$('#github_url').attr('href', data.html_url);
+		})
+	}
+
+	function repos(argument){
+		var name = $('#githubName').val();
+		$.getJSON('https:/api.github.com/users/' + name + '/repos', function(repos){
+			console.log(repos);
+			repos.forEach(function(repo) {
+				$('#repos').append('<li><a href="' + repo.html_url + '">' + repo.name + '</a></li>');
+			})
+		})
+	}
+
+
+});
