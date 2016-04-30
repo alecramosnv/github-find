dfea31178db93bb89bd77a9e5e5002f37221fe28
Alec Ramos <alec1034780@icloud.com>
2016-04-30 11:16:07 -0700

    Add Starred

--- js/find.js
+++ js/find.js
@@ -4,8 +4,8 @@ $(document).ready(function(){
 	$('#result').hide();
 
 	function results(event) {
-		 profile();
-		 repos(); 
+		profile();
+		repos(); 
 	}
 
 	function profile (event) {
@@ -17,16 +17,28 @@ $(document).ready(function(){
 			$('#name').text(data.name);
 			$('#username').text(data.login);
 			$('#followers').text(data.followers);
+			$('#followers').attr('href', 'https://github.com/' + name + '/followers');
 			$('#following').text(data.following);
+			$('#following').attr('href', 'https://github.com/' + name + '/following');
+			$('#starred').attr('href', 'https://github.com/stars/' + name);
 			$('#avatar').attr('src', data.avatar_url);
 			$('#blog_url').attr('href', data.blog);
 			$('#github_url').attr('href', data.html_url);
 		})
 	}
 
+	$('#githubName').on("keypress", function (e) {
+		if (e.which === 13) {
+			results();
+			e.preventDefault();
+			$('#githubName').attr('value', '');
+		}s
+	});
+
 	function repos(argument){
 		var name = $('#githubName').val();
 		$.getJSON('https:/api.github.com/users/' + name + '/repos', function(repos){
+			$('#repos').empty();
 			console.log(repos);
 			repos.forEach(function(repo) {
 				$('#repos').append('<li><a href="' + repo.html_url + '">' + repo.name + '</a></li>');
